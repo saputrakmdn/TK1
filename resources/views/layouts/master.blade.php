1ce9297dbfcaeba309c0b02dc3372b893d96e6ad
saputrakmdn <saputra.kmdn@gmail.com>
2023-06-16 21:35:20 +0700

    update minor

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -7,7 +7,6 @@
     <meta name="author" content=""/>
     <title>TK1</title>
     <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="{{asset('template/assets/favicon.ico')}}"/>
     <!-- Font Awesome icons (free version)-->
     <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
     <!-- Google fonts-->
